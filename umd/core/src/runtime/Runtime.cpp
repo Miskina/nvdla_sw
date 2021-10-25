b3ecc1da19352b737ee4547507211877d23ea26d
Miskina <mhlkv02@gmail.com>
2021-10-25 14:57:35 +0200

    Removed unnecessary vector

--- umd/core/src/runtime/Runtime.cpp
+++ umd/core/src/runtime/Runtime.cpp
@@ -428,7 +428,6 @@ void Runtime::unload()
 //
 bool Runtime::bindInputTensor(int index, void *hMem)
 {
-    std::vector<Memory *>bind_to_mem;
     bool ok = true;
     if ( index < 0 ) {
         ok = false;
@@ -438,14 +437,9 @@ bool Runtime::bindInputTensor(int index, void *hMem)
     // determine which mem needs to be rebound
     for ( size_t mi = 0, MI = m_memory.size(); mi != MI; ++mi ) {
         if ( m_memory[mi].inputBindId() == index ) {
-            bind_to_mem.push_back( &m_memory[mi] );
+            m_memory[mi].setHandle(hMem);
+            m_memory[mi].setVirtAddr(m_hmem_memory_map[hMem]);
         }
-    }
-
-    // unlikely to be > size 1, but...
-    for (size_t bmi = 0, BMI = bind_to_mem.size(); bmi != BMI; ++bmi ) {
-        bind_to_mem[bmi]->setHandle(hMem);
-        bind_to_mem[bmi]->setVirtAddr(m_hmem_memory_map[hMem]);
     }
 
  done:
